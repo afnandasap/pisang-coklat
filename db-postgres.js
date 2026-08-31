f3c833aef9a5eca950b64714c397e364d8c0244b
pisang-coklat <afnanda.iqbal@gmail.com>
2026-08-31 20:32:18 +0700

    Use Railway DATABASE_URL

--- db-postgres.js
+++ db-postgres.js
@@ -3,11 +3,7 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    host: process.env.PGHOST,
-    port: Number(process.env.PGPORT),
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE
+    connectionString: process.env.DATABASE_URL
 });
 
 module.exports = pool;
