20889faf0e6669c3010b7241bf9c84eb61b76e66
pisang-coklat <afnanda.iqbal@gmail.com>
2026-08-31 20:20:30 +0700

    Fix Railway PostgreSQL and Redis connection

--- db-postgres.js
+++ db-postgres.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE
+    host: process.env.PGHOST,
+    port: Number(process.env.PGPORT),
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE
 });
 
 module.exports = pool;
