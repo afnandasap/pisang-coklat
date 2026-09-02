c152743bbaf54dcb187a82d57b9d90d84555b8ea
pisang-coklat <afnanda.iqbal@gmail.com>
2026-09-02 13:33:40 +0700

    fix railway postgres connection

--- db-postgres.js
+++ db-postgres.js
@@ -2,14 +2,22 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
-console.log("DATABASE_URL ada:", !!process.env.DATABASE_URL);
-
-if (!process.env.DATABASE_URL) {
-    throw new Error("DATABASE_URL tidak tersedia");
-}
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : false
+    })
+  : new Pool({
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+      ssl: process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : false
+    });
 
 module.exports = pool;
